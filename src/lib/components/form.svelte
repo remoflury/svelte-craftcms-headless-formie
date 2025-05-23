fd8e173072b02b96bba6ec04b1f657c7e3a8e133
remoflury <remoflury97+web@gmail.com>
2025-05-23 16:10:05 +0200

    feat: add prop

--- src/lib/components/form.svelte
+++ src/lib/components/form.svelte
@@ -5,27 +5,27 @@
 	import { getFormMutation, getMutationVariables } from '$lib/utils/mutationUtils.js';
 	import { FormQuery } from '$lib/queries/FormQuery.js';
 	import Field from './field.svelte';
-	// import Button from '../button/button.svelte';
-	// import ErrorMsg from '../fetching/errorMsg.svelte';
-	import SubmissionMessage from './submissionMessage.svelte';
-	// import Skeleton from '../fetching/skeleton.svelte';
 
 	type Props = {
 		handle: string;
 		submitButton: Snippet;
 		isLoading?: boolean;
-		onsuccessfulsubmit?: () => void;
+		onsuccessfulsubmit?: (message: string | null) => void;
+		onerror?: (message: string | null) => void;
 		skeletonSnippet?: Snippet;
 		errorSnippet?: Snippet;
+		afterSubmitSnippet?: Snippet; // component, which can be shown after submission
 		recaptchaKey?: string;
 		publicCmsApi: string;
 	};
 	let {
 		handle,
 		onsuccessfulsubmit,
+		onerror,
 		errorSnippet,
 		skeletonSnippet,
 		isLoading = $bindable(false),
+		afterSubmitSnippet,
 		submitButton,
 		recaptchaKey,
 		publicCmsApi
@@ -127,18 +127,10 @@
 		if (json.data && !json.errors) {
 			isSuccessful = true;
 			successMessage = formData?.form?.settings?.submitActionMessageHtml;
-			onsuccessfulsubmit?.();
+			onsuccessfulsubmit?.(successMessage);
 		} else {
-			// // eslint-disable @typescript-esling/no-unused-vars
-			// let message = $state('');
-			// if (json.errors[0].message.includes('Email is not a valid email')) {
-			//   // eslint-disable @typescript-esling/no-unused-vars
-			//   message = 'Bitte gib eine gültige E-Mail-Adresse ein.';
-			// } else {
-			//   // eslint-disable @typescript-esling/no-unused-vars
-			//   message = formData?.form?.settings?.submitErrorMessageHtml;
-			// }
 			errorMessage = json.errors[0].message;
+			onerror?.(errorMessage);
 		}
 		isLoading = false;
 	});
@@ -155,23 +147,19 @@
 	{/if}
 {:then formData}
 	<form
-		class="col-span-full md:col-span-6 lg:col-span-8"
 		bind:this={form}
 		id={formData.form.handle}
 		onsubmit={async (e) => await onSubmit(e, formData)}
 	>
 		{#each formData.form.pages as page, i (page.id)}
-			<div class="space-y-6" id={page.id} class:hidden={i !== pageIndex || successMessage}>
+			<div id={page.id} class:hidden={i !== pageIndex || successMessage}>
 				{#if page.rows.length > 0}
 					<!-- eslint-disable-next-line -->
 					{#each page.rows as row (crypto.randomUUID())}
 						<!-- eslint-disable-next-line -->
 						{#each row.rowFields as field (crypto.randomUUID())}
 							{#if checkFieldConditions(field.conditions, formFields)}
-								<div
-									class="field relative grow {row.rowFields.length > 1 && 'basis-[40%]'}"
-									data-field-wrapper
-								>
+								<div class="field relative grow {row.rowFields.length > 1 && 'basis-[40%]'}">
 									<Field {field} {updateFormFields} />
 								</div>
 							{/if}
@@ -182,18 +170,10 @@
 		{/each}
 		{#if !isSuccessful}
 			{@render submitButton()}
-			<!-- <div class="mt-4 lg:mt-6 2xl:mt-8">
-				<Button type="submit" disabled={isLoading} active={true} aria-busy={isLoading}
-					>{formData.form.pages[0].settings.submitButtonLabel}</Button
-				>
-			</div> -->
 		{/if}
 
-		{#if successMessage}
-			<SubmissionMessage text={successMessage} success={true} />
-		{/if}
-		{#if errorMessage}
-			<SubmissionMessage text={errorMessage} success={false} />
+		{#if afterSubmitSnippet}
+			{@render afterSubmitSnippet()}
 		{/if}
 
 		<small class="mt-4 block w-full text-xs">
