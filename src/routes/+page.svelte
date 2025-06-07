03941392e8209a6ae78384e29ad86d404d643699
remoflury <remoflury97+web@gmail.com>
2025-06-07 17:15:21 +0200

    chore: cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { PUBLIC_CMS_API } from '$env/static/public';
 	import { FormieForm, type AfterSubmitState } from '$lib/index.js';
 
@@ -50,39 +50,10 @@
 			<p>This is an error message</p>
 		{/snippet}
 	</FormieForm>
-	<!-- <FormieForm
-		handle={form[0].handle}
-		publicCmsApi={PUBLIC_CMS_API}
-		onsuccessfulsubmit={() => console.log('success')}
-		onerror={() => console.log('error')}
-		bind:isLoading
-		bind:submitButtonText
-		bind:afterSubmitState
-	>
-		{#snippet skeletonSnippet()}
-			this is a skeleton fallback
-		{/snippet}
-	
-		{#snippet submitButton()}
-			<button>{submitButtonText}</button>
-		{/snippet}
-	
-		{#snippet afterSubmitSnippet()}
-			{#if afterSubmitState}
-				<p style="color: {afterSubmitState.isSuccess ? 'green' : 'red'}">
-					{afterSubmitState.message}
-				</p>
-			{/if}
-		{/snippet}
-	
-		{#snippet errorSnippet()}
-			<p>This is an error message</p>
-		{/snippet}
-	</FormieForm> -->
 </section>
 
 <style>
 	section {
 		margin-inline: 2rem;
 	}
-</style>
+</style> -->
